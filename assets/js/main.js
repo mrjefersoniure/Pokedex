3b883de42ee48a9a52d9497d84e7e531f69e6abd
Jeferson Iure <jefersoniurematos@gmail.com>
2024-01-18 21:13:31 -0300

    atualização com popup e status de cada pokemon

---
+++ assets/js/main.js
@@ -0,0 +1,99 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const modal = document.getElementById('modal')
+const modalContent = document.getElementById('modal-content')
+
+const maxRecords = 151
+const limit = 10;
+let offset = 0;
+
+
+
+
+function openModal(pokemon) {
+    modal.style.display = 'block';
+    // Atualize o conteúdo do modal com as informações do Pokémon
+    modalContent.innerHTML = `
+        <section class="pokemon_detail">
+            <span class="name">${pokemon.name}</span>
+            <span class="number">#${pokemon.id}</span>
+            <div class="detail ${pokemon.types[0].type.name}">
+              
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            </div>
+            <h3>Base stats</h3>
+              <ol class="stats">
+                    ${pokemon.types.map((pokemon) => `<li class="type">${pokemon.type.name}</li>`).join('')}
+                </ol>
+            <ol class="stats">
+            ${pokemon.stats.map((pokemon) => `
+                <li class="type">${pokemon.stat.name} 
+                <span class="value"><meter value="${pokemon.base_stat}" min="0" max="100">${pokemon.base_stat}%</meter> ${pokemon.base_stat}</span>
+                </li>`
+            ).join('')}
+            </ol>
+        </section>
+
+        <!-- Adicione outras informações do Pokémon conforme necessário -->
+    `;
+}
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li data-idPokemon="${pokemon.number}" class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+                     
+            </div>
+
+        </li>
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+
+loadPokemonItens(offset, limit)
+
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
+
+pokemonList.addEventListener('click', (event) => {
+    
+    const clickedPokemonElement = event.target.closest('.pokemon');
+    const idPokemon = clickedPokemonElement.dataset.idpokemon;
+
+    pokeApi.getPokemonById(idPokemon).then((pokemonStats = []) => {
+      openModal(pokemonStats);
+    })
+       
+    
+});
